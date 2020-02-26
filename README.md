0fb299bfe868ed0f72a7be846fe2a159d105e8bf
BIGV10 <BIGV10@mail.ru>
2020-02-26 13:22:24 +0500

    nothing working

--- README.md
+++
@@ -1,2 +0,0 @@
-# prototype_web
-heroku: https://bigv-postgres.herokuapp.com/api/equipment
---
+++ frontend/src/js/pages/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <equipments-list :equipments="equipments" />
+    </div>
+</template>
+
+<script>
+    import EquipmentList from 'components/equipments/EquipmentList.vue'
+
+    export default {
+        components: {
+            EquipmentList
+        },
+        data() {
+            return {
+                equipments: frontendData.equipments
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
